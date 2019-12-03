117fa2c51754b6662eef4d0a1d1d30c1027da950
Miguel Orjuela <ma.orjuela73@gmail.com>
2019-12-03 03:49:37 -0500

    Se crea el callback para recorte

--- app/app6.py
+++ app/app6.py
@@ -8,6 +8,9 @@ import dash_bootstrap_components as dbc
 import visdcc
 
 from PIL import Image, ImageOps, ImageChops, ImageFilter
+import numpy as np
+import cv2
+import pandas as pd
 
 # external JavaScript files
 external_scripts = [
@@ -66,7 +69,153 @@ table = dbc.Table(table_header + table_body, bordered=True)
 
 image = Image.open('E:\Miguel Orjuela\PROYECTOS\CES App\Samsung.jpg')
 image_bw = image.convert(mode="L")  # Transform to black and white
+
+
 # print(image_bw.size) # [0]: width in pixels [1]: height in pixels
+
+# TODO Ask for the values on the interface (@callback)
+# --- Cambiar contraste en guia de medicion
+def change_contrast(img, level):
+    factor = (259 * (level + 255)) / (255 * (259 - level))
+
+    def contrast(c):
+        return 128 + factor * (c - 128)
+
+    return img.point(contrast)
+
+
+def tomm(w):
+    return -1 / 4 * np.sqrt(5929 - 16 * w * w) + 19.25
+
+
+def calculate_everything(a, b, c, d):
+    if all(v is not None for v in [a, b, c, d]):
+        # bite_params = (500, 600, 2300, 2200)
+        bite_params = (a, b, c, d)
+        guide_params = (30, 2400, 200, 2650)
+        wedge_params = (1050, 2650, 1400, 2950)
+        bite = np.array(image_bw.crop(bite_params))
+        guide = np.array(image_bw.crop(guide_params))
+        wedge = np.array(image_bw.crop(wedge_params))
+        guide_cont = change_contrast(image, 100)
+        guide_cont = np.array(guide_cont.crop(guide_params))
+        guide_cont = cv2.cvtColor(guide_cont, cv2.COLOR_BGR2GRAY)
+        mid = round(guide_cont.shape[0] / 2)
+        zeros = [i for i, e in enumerate(guide_cont[mid]) if e == 0]
+        sum_t = 0
+        count = 0
+        pix = []
+        for i in range(len(zeros) - 1):
+            if ((zeros[i + 1] - zeros[i]) == 1):
+                sum_t = sum_t + zeros[i]
+                count = count + 1
+            else:
+                pix.append(round(sum_t / count))
+                sum_t = 0
+                count = 0
+        mm = round(np.mean(np.diff(pix)))
+
+        wedge_w = []
+
+        for i in range(wedge.shape[0]):
+            x = list(wedge[i])
+            wedge_w.append(max(x))
+        pix_med_y = wedge_w.index(max(wedge_w))
+
+        pix_med_x = np.argmax(wedge[pix_med_y])
+
+        dist = []
+        prof = []
+        dist_p = []
+        for i in range(wedge.shape[1]):
+            dist.append(np.abs(i - pix_med_x) / mm)
+            dist_p.append(np.abs(i - pix_med_x))
+            prof.append(wedge[pix_med_x][i])
+
+        data_wedge_dist = pd.DataFrame({"d_pix": prof, "x_pix": dist_p, "x_mm": dist})
+
+        x = np.arange(0.0, 10, 0.01)
+
+        y = list(map(tomm, x))
+        y_pix = list(map(lambda w: round(w * mm), y))
+        x_pix = list(map(lambda w: round(w * mm), x))
+
+        data_wedge_depth = pd.DataFrame({"x_mill": x, "y_mill": y, "y_pix": y_pix, "x_pix": x_pix})
+
+        wedge_dd = data_wedge_depth.merge(data_wedge_dist)
+        wedge_xd = wedge_dd.groupby("x_pix").median().reset_index()
+        wedge_dd = wedge_dd.groupby("d_pix").median().reset_index()
+
+        k_pix = min(np.array(wedge_dd[wedge_dd.y_mill <= 0.05]["d_pix"]))
+        w_pix = min(np.array(wedge_dd[wedge_dd.y_mill <= 0.35]["d_pix"]))
+
+        # Metodo de corte de área para analizar todo lo que está adentro
+
+        # bite = np.array(image.crop(bite_params))
+        cut_params = [[150, 1100], [220, 820], [450, 800], [400, 1200]]
+        pts = np.array(cut_params)
+        ## (1) Crop the bounding rect
+        rect = cv2.boundingRect(pts)
+        x, y, w, h = rect
+        croped = bite[y:y + h, x:x + w].copy()
+
+        ## (2) make mask
+        pts = pts - pts.min(axis=0)
+
+        mask = np.zeros(croped.shape[:2], np.uint8)
+        cv2.drawContours(mask, [pts], -1, (255, 255, 255), -1, cv2.LINE_AA)
+
+        ## (3) do bit-op
+        dst = cv2.bitwise_and(croped, croped, mask=mask)
+
+        class Switch(dict):
+            def __getitem__(self, item):
+                for key in self.keys():  # iterate over the intervals
+                    if item in key:  # if the argument is part of that interval
+                        return super().__getitem__(key)  # return its associated value
+                raise KeyError(item)  # if not in any interval, raise KeyError
+
+        switch = Switch({
+            range(k_pix, 255): 2,
+            range(w_pix, k_pix): 1,
+            range(0, w_pix): 0
+        })
+
+        classification = dst.copy()
+        for i in range(dst.shape[0]):
+            classification[i] = [switch[x] for x in dst[i]]
+
+        low = 0
+        contact = 0
+        close = 0
+        for i in range(classification.shape[0]):
+            for j in range(classification.shape[1]):
+                if (classification[i][j] == 0):
+                    low = low + 1
+                if (classification[i][j] == 1):
+                    contact = contact + 1
+                if (classification[i][j] == 2):
+                    close = close + 1
+        resultDiv = html.Div([
+            html.P('bite shape: ' + str(bite.shape)),
+            html.P('guide shape: ' + str(guide.shape)),
+            html.P('wedge shape: ' + str(wedge.shape)),
+            html.P('guide_cont shape: ' + str(guide_cont.shape)),
+            html.P('mm: ' + str(mm)),
+            html.P('pix_med_x: ' + str(pix_med_x)),
+            html.P('pix_med_y: ' + str(pix_med_y)),
+            html.P('k_pix: ' + str(k_pix)),
+            html.P('w_pix: ' + str(w_pix)),
+            html.P('low: ' + str(low)),
+            html.P('contact: ' + str(contact)),
+            html.P('close: ' + str(close)),
+        ])
+        return resultDiv
+    return html.Div("Algún campo es nulo")
+
+
+# TODO Por aquí vamos
+
 
 app.layout = html.Div([
     dcc.Store(id='memory-output'),
@@ -201,10 +350,13 @@ app.layout = html.Div([
                 html.H3('Paso #3'),
                 html.H2('Cálculos y configuración de áreas para análisis de profundidad.'),
                 html.P('A continuación se imprimen gráficos de prueba para verificar el proceso.'),
-                # TODO: Imprimir print(image_bw.size)
                 html.P("Table placeholder", id='coord-x-ftw'),
                 html.H4("Dimensiones de la imagen"),
                 html.P(image_bw.size),
+                html.Button(id='submit-button', n_clicks=0, children='Submit'),
+                html.Div(id='calculate-everything-output'),
+                # TODO: Por aquí vamos
+
             ])
         ]),
         dcc.Tab(label='Paso #4', children=[
@@ -282,7 +434,6 @@ def parse_contents_to_cropper(contents, filename, date):
                Input('inicioy-input', 'value'),
                ])
 def updateholita(a, b, c, d):
-
     atable_header = [
         html.Thead(html.Tr([html.Th("Coordenada"), html.Th("Valor")]))
     ]
@@ -295,6 +446,17 @@ def updateholita(a, b, c, d):
     atable_body = [html.Tbody([arow1, arow2, arow3, arow4])]
 
     return dbc.Table(atable_header + atable_body, bordered=True)
+
+
+@app.callback(Output('calculate-everything-output', 'children'),
+              [Input('submit-button', 'n_clicks')],
+              [State('ancho-input', 'value'),
+               State('alto-input', 'value'),
+               State('iniciox-input', 'value'),
+               State('inicioy-input', 'value'),
+               ])
+def update_coordenates(n_clicks, a, b, c, d):
+    return calculate_everything(float(a), float(b), float(c), float(d))
 
 
 @app.callback(Output('output-image-upload', 'children'),
