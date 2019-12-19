f1f4982390f8ab73aed3e8c2a46ef97dac84301b
Miguel Orjuela <ma.orjuela73@gmail.com>
2019-12-18 23:28:00 -0500

    Correccion app7 codigo que no se usaba

--- app/app7.py
+++ app/app7.py
@@ -241,39 +241,6 @@ def update_output(list_of_contents, list_of_names, list_of_dates):
         children = [
             parse_contents(c, n, d) for c, n, d in
             zip(list_of_contents, list_of_names, list_of_dates)]
-        figure = dict(
-            data=[
-                dict(
-                    x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
-                       2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
-                    y=[219, 146, 112, 127, 124, 180, 236, 207, 236, 263,
-                       350, 430, 474, 526, 488, 537, 500, 439],
-                    name='Rest of world',
-                    marker=dict(
-                        color='rgb(55, 83, 109)'
-                    )
-                ),
-                dict(
-                    x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
-                       2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
-                    y=[16, 13, 10, 11, 28, 37, 43, 55, 56, 88, 105, 156, 270,
-                       299, 340, 403, 549, 499],
-                    name='China',
-                    marker=dict(
-                        color='rgb(26, 118, 255)'
-                    )
-                )
-            ],
-            layout=dict(
-                title='US Export of Plastic Scrap',
-                showlegend=True,
-                legend=dict(
-                    x=0,
-                    y=1.0
-                ),
-                margin=dict(l=40, r=0, t=40, b=30)
-            )
-        )
         figure = create_image_figure(list_of_names[0])
         return children, figure
 
