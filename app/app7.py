28198ee8523de3e68f39896c7cb3be7883503d31
Dorita Suárez <dmsuarezv@gmail.com>
2020-08-14 03:22:48 -0500

    Changes Algorithm added

--- app/app7.py
+++ app/app7.py
@@ -315,207 +315,36 @@ def create_figure_cropped_lasso(image_path, xs, ys):
     return fig
 
 
-# def calculo(image_path, bite_params, guide_params, wedge_params, x, y):
-#     # print(cut_params)
-#     # cut_params = [(150,1100),(220,820),(450.40,800),(400,1200.18)]
-#     image = Image.open(image_path)
-#     image_bw = image.convert(mode="L")  # Transform to black and white
-
-#     imagen_bit = image_bw.crop(bite_params)
-
-#     bite = np.array(image_bw.crop(bite_params))
-#     guide = np.array(image_bw.crop(guide_params))
-#     wedge = np.array(image_bw.crop(wedge_params))
-
-#     imo_w, imo_h = imagen_bit.size
-#     img_width = imo_w
-#     img_height = imo_h
-#     scale_factor_a = 750 / imo_w
-#     x = list(map(lambda x: round(x * 1 / scale_factor_a), x))
-#     y = list(map(lambda x: round(img_height - x * 1 / scale_factor_a), y))
-#     cut_params = list(zip(x, y))
-#     print(cut_params)
-
-#     def change_contrast(img, level):
-#         factor = (259 * (level + 255)) / (255 * (259 - level))
-
-#         def contrast(c):
-#             return 128 + factor * (c - 128)
-
-#         return img.point(contrast)
-
-#     guide_cont = change_contrast(image, 100)
-#     guide_cont = np.array(guide_cont.crop(guide_params))
-#     guide_cont = cv2.cvtColor(guide_cont, cv2.COLOR_BGR2GRAY)
-
-#     mid = round(guide_cont.shape[0] / 2)
-#     print(guide_cont[mid])
-#     print("mid")
-#     # print(mid)
-#     print(guide_cont.shape)
-#     zeros = [i for i, e in enumerate(guide_cont[mid]) if e == 0]
-#     print('ceros')
-#     print(np.diff(zeros))
-
-#     sum_t = 0
-#     count = 0
-#     pix = []
-#     for i in range(len(zeros) - 1):
-#         if ((zeros[i + 1] - zeros[i]) == 1):
-#             sum_t = sum_t + zeros[i]
-#             count = count + 1
-#         else:
-#             pix.append(round(sum_t / count))
-#             sum_t = 0
-#             count = 0
-#     print("pix")
-#     print(pix)
-#     mm = round(np.mean(np.diff(pix)))
-
-#     wedge_w = []
-#     for i in range(wedge.shape[0]):
-#         x = list(wedge[i])
-#         wedge_w.append(max(x))
-#     pix_med_y = wedge_w.index(max(wedge_w))
-#     #     plt.imshow(wedge)
-
-#     pix_med_x = np.argmax(wedge[pix_med_y])
-
-#     dist = []
-#     prof = []
-#     dist_p = []
-#     for i in range(wedge.shape[1]):
-#         dist.append(np.abs(i - pix_med_x) / mm)
-#         dist_p.append(np.abs(i - pix_med_x))
-#         prof.append(wedge[pix_med_x][i])
-#     #     plt.plot(dist_p, prof)
-#     data_wedge_dist = pd.DataFrame({"d_pix": prof, "x_pix": dist_p, "x_mm": dist})
-
-#     x = np.arange(-19, 19, 0.01)
-
-#     def tomm(w):
-#         return -np.sqrt(361 - w * w) + 19
-
-#     #     return -1/2*np.sqrt(22201-4*w*w)
-#     y = list(map(tomm, x))
-#     y_pix = list(map(lambda w: round(w * mm), y))
-#     x_pix = list(map(lambda w: round(w * mm), x))
-#     data_wedge_depth = pd.DataFrame({"x_mill": x, "y_mill": y, "y_pix": y_pix, "x_pix": x_pix})
-#     wedge_dd = data_wedge_depth.merge(data_wedge_dist)
-#     wedge_xd = wedge_dd.groupby("x_pix").median().reset_index()
-#     wedge_dd = wedge_dd.groupby("d_pix").median().reset_index()
-
-#     k_pix = min(np.array(wedge_dd[wedge_dd.y_mill <= 0.05]["d_pix"]))
-#     w_pix = min(np.array(wedge_dd[wedge_dd.y_mill <= 0.35]["d_pix"]))
-
-#     def depth_found(dist, a):
-#         y = [i for i, e in enumerate(prof) if e == a]
-#         if (len(y) == 0):
-#             return "not found"
-#         else:
-#             return np.median([dist[i] for i in y])
-
-#     distances = []
-#     depths = []
-#     for i in range(255):
-#         distances.append(depth_found(dist, i))
-#         depths.append(i)
-#     for i in range(len(distances)):
-#         if (i == 0 and distances[i] == "not found"):
-#             aux = [x for x in distances if x != "not found"]
-#             distances[i] = max(aux)
-#         if (distances[i] == "not found"):
-#             distances[i] = distances[i - 1]
-
-#     @vectorize([int64(float64)])
-#     def redondear(x):
-#         return x
-
-#     cut_params = redondear(cut_params)
-#     pts = np.array(cut_params)
-#     ## (1) Crop the bounding rect
-#     rect = cv2.boundingRect(pts)
-#     x, y, w, h = rect
-#     croped = bite[y:y + h, x:x + w].copy()
-#     ## (2) make mask
-#     pts = pts - pts.min(axis=0)
-#     mask = np.zeros(croped.shape[:2], np.uint8)
-#     cv2.drawContours(mask, [pts], -1, (255, 255, 255), -1, cv2.LINE_AA)
-#     ## (3) do bit-op
-#     dst = cv2.bitwise_and(croped, croped, mask=mask)
-
-#     print('dst')
-
-#     # print(dst)
-
-#     ## (4) add the white background
-#     # bg = np.ones_like(croped, np.uint8) * 255
-
-#     # cv2.bitwise_not(bg,bg, mask=mask)
-#     # plt.imshow(dst)
-
-#     # class Switch(dict):
-#     #     def __getitem__(self, item):
-#     #         for key in self.keys():  # iterate over the intervals
-#     #             if item in key:  # if the argument is part of that interval
-#     #                 return super().__getitem__(key)  # return its associated value
-#     #         raise KeyError(item)  # if not in any interval, raise KeyError
-#     #
-#     # switch = Switch({
-#     #     range(k_pix, 255): 2,
-#     #     range(w_pix, k_pix): 1,
-#     #     range(0, w_pix): 0
-#     # })
-
-#     def classif(x_ent):
-#         if x_ent < w_pix:
-#             return 0
-#         elif x_ent < k_pix:
-#             return 1
-#         else:
-#             return 2
-
-#     print("k_pix")
-
-#     salida = dst.copy()
-
-#     for i in range(dst.shape[0]):
-#         # print(dst[i])
-#         salida[i] = list(map(lambda x: classif(x), dst[i]))
-#     classification = salida
-
-#     low = 0
-#     contact = 0
-#     close = 0
-#     for i in range(classification.shape[0]):
-#         for j in range(classification.shape[1]):
-#             if (classification[i][j] == 0):
-#                 low = low + 1
-#             if (classification[i][j] == 1):
-#                 close = close + 1
-#             if (classification[i][j] == 2):
-#                 contact = contact + 1
-
-#     area_contact = (1 / mm ** 2) * contact
-#     area_close = (1 / mm ** 2) * close
-
-#     clasi = classification.astype(np.uint8)
-#     ret, thresh = cv2.threshold(clasi, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-#     n_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(thresh)
-
-#     return area_contact, area_close, n_labels
-
-
 def calculo(image_path, bite_params, guide_params, wedge_params, x, y, tooth_index):
     print('--------- BLOQUE calculo iniciado --------------------')
     # print(cut_params)
     # cut_params = [(150,1100),(220,820),(450.40,800),(400,1200.18)]
+    def change_contrast(img, level):
+        factor = (259 * (level + 255)) / (255 * (259 - level))
+        def contrast(c):
+            return 128 + factor * (c - 128)
+        return img.point(contrast)
+    print("Contraste")
     image = Image.open(image_path)
     image_bw = image.convert(mode="L")  # Transform to black and white
     imagen_bit = image_bw.crop(bite_params)
+    print("Creacion de los bits")
+    # BITE
     bite = np.array(image_bw.crop(bite_params))
+    bite_contrast = change_contrast(image, 60)
+    bite_contrast = np.array(bite_contrast.crop(bite_params))
+    bite = np.array(bite_contrast)
+    bite = cv2.cvtColor(bite,cv2.COLOR_BGR2GRAY)
+    print("Cambio de contraste a bite")
+    # GUIDE
     guide = np.array(image_bw.crop(guide_params))
+    # WEDGE
     wedge = np.array(image_bw.crop(wedge_params))
+    wedge_contrast = change_contrast(image, 60)
+    wedge_contrast = np.array(wedge_contrast.crop(wedge_params))
+    wedge = np.array(wedge_contrast)
+    wedge = cv2.cvtColor(wedge,cv2.COLOR_BGR2GRAY)
+    print("Cambio de contrastte a wedge")
     imo_w, imo_h = imagen_bit.size
     img_width = imo_w
     img_height = imo_h
@@ -524,21 +353,19 @@ def calculo(image_path, bite_params, guide_params, wedge_params, x, y, tooth_ind
     y = list(map(lambda x: round(img_height - x * 1 / scale_factor_a), y))
     cut_params = list(zip(x, y))
     print(cut_params)
-    def change_contrast(img, level):
-        factor = (259 * (level + 255)) / (255 * (259 - level))
-        def contrast(c):
-            return 128 + factor * (c - 128)
-        return img.point(contrast)
+    print("Cut params:", cut_params)
+        
     guide_cont = change_contrast(image, 100)
     guide_cont = np.array(guide_cont.crop(guide_params))
     guide_cont = cv2.cvtColor(guide_cont, cv2.COLOR_BGR2GRAY)
+
     mid = round(guide_cont.shape[0] / 2)
     print(guide_cont[mid])
-    print("mid")
+    print("mid:" , mid)
     # print(mid)
     print(guide_cont.shape)
     zeros = [i for i, e in enumerate(guide_cont[mid]) if e == 0]
-    print('ceros')
+    print('ceros:', zeros)
     print(np.diff(zeros))
     sum_t = 0
     count = 0
@@ -554,13 +381,60 @@ def calculo(image_path, bite_params, guide_params, wedge_params, x, y, tooth_ind
     print("pix")
     print(pix)
     mm = round(np.mean(np.diff(pix)))
+    print("mm: ", mm)
+
+    ## CALIBRATE WEDGE
+    print("CALIBRATE WEDGE")
+    wedge_mask = wedge.copy()
+    mins = []
+    maxs = []
+    for i in range(wedge.shape[0]):
+        wedge_mask[i] = np.where(wedge[i] == 0, 0, 1)
+        if(sum(wedge_mask[i])!= 0):
+            mins.append(np.min(np.where(wedge_mask[i] == 1)[0]))
+            maxs.append(np.max(np.where(wedge_mask[i] == 1)[0]))
+
+    cut1 = np.min(mins)-mm
+    cut3 = np.max(maxs)+mm
+    print("CALIBRATE WEDGE")
+
+    mins_y = []
+    maxs_y = []
+    for i in range(wedge.shape[1]):
+        if(sum(wedge_mask[:, i])!= 0):
+            mins_y.append(np.min(np.where(wedge_mask[:,i] == 1)[0]))
+            maxs_y.append(np.max(np.where(wedge_mask[:,i] == 1)[0]))
+
+    cut2 = np.min(mins_y)-mm
+    cut4 = np.max(maxs_y)+mm
+
+    wedge_params1 = (int(wedge_params[0] + cut1),
+                 int(wedge_params[1] + cut2),
+                 int(wedge_params[2] - (wedge_params[2] - wedge_params[0] - cut3)),
+                 int(wedge_params[3] - (wedge_params[3] - wedge_params[1] - cut4)))
+    print("CALIBRATE WEDGE")
+
+    wedge_contrast = change_contrast(image, 60)
+    wedge_contrast = np.array(wedge_contrast.crop(wedge_params1))
+    wedge = np.array(wedge_contrast)
+    wedge = cv2.cvtColor(wedge,cv2.COLOR_BGR2GRAY)
+
+    ### END WEDGE
+    ### CONVERT DEPTH PIXEL TO MILLIMETERS
     wedge_w = []
     for i in range(wedge.shape[0]):
         x = list(wedge[i])
         wedge_w.append(max(x))
     pix_med_y = wedge_w.index(max(wedge_w))
-    #     plt.imshow(wedge)
+
+    wedge_w_x = []
+
+    for i in range(wedge.shape[1]):
+        x = list(wedge[:, i])
+        wedge_w_x.append(max(x))
+    pix_med_x = wedge_w_x.index(max(wedge_w_x))
     pix_med_x = np.argmax(wedge[pix_med_y])
+
     dist = []
     prof = []
     dist_p = []
@@ -569,40 +443,45 @@ def calculo(image_path, bite_params, guide_params, wedge_params, x, y, tooth_ind
         dist_p.append(np.abs(i - pix_med_x))
         prof.append(wedge[pix_med_x][i])
     #     plt.plot(dist_p, prof)
-    data_wedge_dist = pd.DataFrame({"d_pix": prof, "x_pix": dist_p, "x_mm": dist})
+    data_wedge_dist_x = pd.DataFrame({"d_pix": prof, "x_pix": dist_p, "x_mm": dist})
+    print("data wedge dist")
+    dist_y = []
+    prof_y = []
+    dist_p_y = []
+    for i in range(wedge.shape[0]):
+            dist_y.append((i - pix_med_y)/mm)
+            dist_p_y.append((i - pix_med_y))
+            prof_y.append(wedge[i][pix_med_y])
+
+    data_wedge_dist_y = pd.DataFrame({"d_pix":prof_y, "x_pix":dist_p_y, "x_mm":dist_y})
+
+    data_wedge_dist = data_wedge_dist_x.append(data_wedge_dist_y)
+    # plt.plot(data_wedge_dist["x_mm"], data_wedge_dist["d_pix"])
+    Promedios = data_wedge_dist.groupby("x_mm").mean().reset_index()
+    print("Promedios: ", Promedios)
+
     x = np.arange(-19, 19, 0.01)
     def tomm(w):
         return -np.sqrt(361 - w * w) + 19
-    #     return -1/2*np.sqrt(22201-4*w*w)
-    y = list(map(tomm, x))
-    y_pix = list(map(lambda w: np.abs(round(w * mm)), y))
-    x_pix = list(map(lambda w: np.abs(round(w * mm)), x))
-    data_wedge_depth = pd.DataFrame({"x_mill": x, "y_mill": y, "y_pix": y_pix, "x_pix": x_pix})
-    wedge_dd = data_wedge_depth.merge(data_wedge_dist)
-    wedge_xd = wedge_dd.groupby("x_pix").median().reset_index()
-    wedge_dd = wedge_dd.groupby("d_pix").median().reset_index()
-    k_pix = min(np.array(wedge_dd[wedge_dd.y_mill <= 0.05]["d_pix"]))
-    w_pix = min(np.array(wedge_dd[wedge_dd.y_mill <= 0.35]["d_pix"]))
-    def depth_found(dist, a):
-        y = [i for i, e in enumerate(prof) if e == a]
-        if (len(y) == 0):
-            return "not found"
-        else:
-            return np.median([dist[i] for i in y])
-    distances = []
-    depths = []
-    for i in range(255):
-        distances.append(depth_found(dist, i))
-        depths.append(i)
-    for i in range(len(distances)):
-        if (i == 0 and distances[i] == "not found"):
-            aux = [x for x in distances if x != "not found"]
-            distances[i] = max(aux)
-        if (distances[i] == "not found"):
-            distances[i] = distances[i - 1]
+    
+    Promedios["y_mm"] = list(map(tomm, Promedios["x_mm"]))
+    Maximos = Promedios.groupby("y_mm").max().reset_index()
+
+    x_temp = np.array(Maximos["y_mm"])
+    val_pix = [0]
+    val_mm = [0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05, 0.005]
+
+    for i in val_mm:
+        temp = np.max(Maximos["d_pix"][np.where(x_temp >= i)[0]])
+        print(temp)
+        val_pix.append(temp)
+        
+    val_pix.append(256)
+
     @vectorize([int64(float64)])
     def redondear(x):
         return x
+
     cut_params = redondear(cut_params)
     pts = np.array(cut_params)
     ## (1) Crop the bounding rect
@@ -616,51 +495,77 @@ def calculo(image_path, bite_params, guide_params, wedge_params, x, y, tooth_ind
     ## (3) do bit-op
     dst = cv2.bitwise_and(croped, croped, mask=mask)
     print('dst')
-### PATCH 1.0 INICIO
-    rows = dst.shape[0]
-    cols = dst.shape[1]
-    classification_contact = np.array([([0]*cols) for i in range(rows)])
-    classification_close = np.array([([0]*cols) for i in range(rows)])
-    classification = np.array([([0]*cols) for i in range(rows)])
-    for i in range(rows):
-        for j in range(cols):
-            if dst[i][j] >= w_pix and dst[i][j] < k_pix:
-                classification[i][j] = 1 ##// Close
-                classification_close[i][j] = 1
-            if dst[i][j] >= k_pix:
-                classification[i][j] = 2 ##// contact
-                classification_contact[i][j] = 1
+    ## PATCH 2.0 INICIO
+    def clasif(value, arr):
+        for i in range(len(arr)-1):
+            if value >= arr[i] and value <= arr[i+1]:
+                return i
+    
+    dst_clasi = dst.copy()
+    for i in range(dst_clasi.shape[0]):
+        for j in range(dst_clasi.shape[1]):
+            dst_clasi[i][j] = clasif(dst[i][j], val_pix)
+    
+    classification = dst_clasi
 
-    def blobs_function(ToClass, mm, class_cont, tooth_id, tooth_name):
+    def maskContact(matClas, pat):
+        matClasRes = matClas.copy()
+        for i in range(matClas.shape[0]):
+            matClasRes[i] = np.where(matClas[i] == pat, 1, 0)
+        return matClasRes
+
+    areas_class = []
+    for k in range(len(val_pix)-1):
+        areas_class.append(sum(sum(maskContact(classification, k)))/mm**2)
+    # areas_class
+    val_mic = [">350", "350 - 300", "300 - 250", "250 - 200", "200 - 150", "150 - 100", "100 - 50", "50 - 5", "<5"]
+    to_class_area = pd.DataFrame({'tooth_id': "Tooth # {}".format(tooth_index), "contact_id" : val_mic , "contact_area" : areas_class}).reset_index()
+    to_class_area["class"] = to_class_area["index"]
+    print("val_mic: " , val_mic)
+
+    def blobs(ToClass, mm, class_cont, tooth_id):
         blobs, num = ndimage.label(ToClass)
+        blobx_mm = []
         blobx_pix = []
         blobx_num = []
     #     temp2 = sum(sum(classification_close))
         if num > 0:
             for i in range(num):
                 c_temp = (sum(sum(blobs == (i+1))))/(mm**2)
-                print(c_temp)
-                blobx_pix.append(c_temp)
-                blobx_num.append(str(i+1))
-        Total_area = sum(blobx_pix)
+                p_temp = (sum(sum(blobs == (i+1))))
+    #             print(c_temp)
+                if c_temp > 0.05:
+                    blobx_mm.append(c_temp)
+                    blobx_pix.append(p_temp)
+                    blobx_num.append(str(i+1))
+        Total_area = sum(blobx_mm)
+        Total_pix = sum(blobx_pix)
         blobx_pix.append(sum(blobx_pix))
+        blobx_mm.append(sum(blobx_mm))
         blobx_num.append("Total " + class_cont)
-        blobx_pix_df = pd.DataFrame({'tooth_id': tooth_id, 'contact_id':blobx_num, 'contact_area': blobx_pix, 'class': class_cont})
+        blobx_pix_df = pd.DataFrame({'tooth_id': tooth_id, 'contact_id':blobx_num, 'contact_pixel': blobx_pix, 'contact_area': blobx_mm, 'class': class_cont})
         return blobx_pix_df, num, Total_area
 
-    tooth_results = pd.DataFrame()
-    table_contact, n_labels, area_cont = blobs_function(classification_contact,
-                                                          mm,
-                                                          "Contact",
-                                                          "Tooth # {}".format(tooth_index),
-                                                          "Diente1")
-    table_close, quant_close, area_close = blobs_function(classification_close,
-                                                          mm,
-                                                          "Close",
-                                                          "Tooth # {}".format(tooth_index),
-                                                          "Diente1")
-    tooth_results = tooth_results.append(table_contact).append(table_close)
+    results = pd.DataFrame()
+    area_cont = 0
+    area_close = 0
+    n_labels = 0
 
+    for i in range(to_class_area.shape[0]):
+        classification_temp = maskContact(classification, i)
+        class_temp = to_class_area["contact_id"][i]
+        table_contact, quant_cont, area_cont = blobs(classification_temp, mm, class_temp, "Tooth # {}".format(tooth_index))
+        if(i == 8):
+            area_cont = area_cont + area_cont
+            n_labels = n_labels + quant_cont
+        else:
+            area_close = area_close + area_cont
+        results = results.append(table_contact)
+
+    print(to_class_area)
+    area_close = to_class_area["contact_area"][8]
+    area_close = sum(to_class_area["contact_area"][list(range(1, 7))])
+    tooth_results = to_class_area
     return area_cont, area_close, n_labels, tooth_results
 
 # area_contact, area_close, n_labels
